099e03c1af9397819c22ca9cd824bf847adf65a4
Alex Ameen <alex.ameen.tx@gmail.com>
2022-02-23 19:08:57 -0600

    remove reference to Nix flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
   description = "Add X11 tool set_wm_class";
-  inputs.nixpkgs.follows = "nix/nixpkgs";
-  outputs = { self, nix, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }: {
 
     overlays.set_wm_class = import ./overlay.nix;
     overlay = self.overlays.set_wm_class;
 
     packages.x86_64-linux.set_wm_class = ( import nixpkgs {
       sys = "x86_64-linux";
-      overlays = [self.overlay nix.overlay];
+      overlays = [self.overlay];
     } ).set_wm_class;
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.set_wm_class;
 
