2fe2ab03f4376a4fdcef11c416f6a9cbdd02d1ba
Alex Ameen <alex.ameen.tx@gmail.com>
2022-09-17 12:27:07 -0500

    drop support for legacy style overlays

--- flake.nix
+++ flake.nix
@@ -1,19 +1,37 @@
 {
+
   description = "Add X11 tool set_wm_class";
-  outputs = { self, nixpkgs, ... }: {
+
+  outputs = { self, nixpkgs, ... }: let
+    supportedSystems = [
+      "x86_64-linux"  "aarch64-linux"
+      "x86_64-darwin" "aarch64-darwin"
+    ];
+    eachSupportedSystemMap = fn:
+      builtins.listToAttrs ( map ( name: {
+        inherit name;     # System arch/plat pair
+        value = fn name;
+      } ) supportedSystems );
+  in {
 
     overlays.set_wm_class = import ./overlay.nix;
-    overlay = self.overlays.set_wm_class;
+    overlays.default      = self.overlays.set_wm_class;
 
-    packages.x86_64-linux.set_wm_class = ( import nixpkgs {
-      sys = "x86_64-linux";
-      overlays = [self.overlay];
-    } ).set_wm_class;
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.set_wm_class;
+    packages = eachSupportedSystemMap ( system: {
+      set_wm_class = ( import nixpkgs {
+        overlays = [self.overlays.default];
+        inherit system;
+      } ).set_wm_class;
+      default = self.packages.${system}.set_wm_class;
+    } );
 
-    nixosModules.set_wm_class = { pkgs, ... }: {
-      nixpkgs.overlays = [self.overlay];
+    nixosModules = {
+      set_wm_class = { ... }: {
+        nixpkgs.overlays = [self.overlay];
+      };
+      default = self.nixosModules.set_wm_class;
     };
-    nixosModule = self.nixosModules.set_wm_class;
+
   };
+
 }
